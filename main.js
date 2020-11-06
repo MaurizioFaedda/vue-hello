549675b710ddaf6c28d049b528097ed442a627b5
Maurizio <maurizio.faedda1@gmail.com>
2020-11-06 16:48:27 +0100

    Img casuale pescata dalla mia cartella

--- main.js
+++ main.js
@@ -5,7 +5,14 @@ var root = new Vue({
     data: {
         text: 'hello world',
         img1: 'images/natura1.jpg',
-        img2: 'natura2.jpg'
+        img2: 'natura2.jpg',
+        img3: 'images/natura3.jpg'
 
-    }
+    },
+
+    methods: {
+        randomNumber : function(){
+            return Math.floor(Math.random() * (6)) + 1;
+        }
+    },
 })
