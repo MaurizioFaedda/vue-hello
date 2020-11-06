9eea295a59f2ef3c183bdd6794f7d6d486e0e800
Maurizio <maurizio.faedda1@gmail.com>
2020-11-06 17:35:46 +0100

    Al click del bottone genero un numero random abbinato a una img

--- main.js
+++ main.js
@@ -6,13 +6,18 @@ var root = new Vue({
         text: 'hello world',
         img1: 'images/natura1.jpg',
         img2: 'natura2.jpg',
-        img3: 'images/natura3.jpg'
+        img3: 'images/natura3.jpg',
+        // creo questo dato per aiutarmi a mettere dentro il numero random
+        number: '1'
+
 
     },
 
     methods: {
+
         randomNumber : function(){
             return Math.floor(Math.random() * (5)) + 1;
-        }
+        },
+
     },
 })
